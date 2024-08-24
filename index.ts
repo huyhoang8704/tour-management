f2cdd2a750b7e2c274cc9a1bcdac1477c0b7450d
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2024-08-25 00:11:12 +0700

    Pug

--- index.ts
+++ index.ts
@@ -3,7 +3,8 @@ import express , { Express , Request , Response } from "express";
 const app : Express  = express()
 const port : number | string = process.env.PORT || 3002;
 
-
+app.set('views','./views');
+app.set('view engine', 'pug');
 
 
 app.get('/',(req : Request, res : Response) => {
