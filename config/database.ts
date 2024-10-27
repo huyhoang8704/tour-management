9cec809a8d391edac9fe840c26bc61f8da1ff76b
Nguyen Huy Hoang <137654109+huyhoang8704@users.noreply.github.com>
2024-10-28 03:39:16 +0700

    sequelize connect mysql

--- config/database.ts
+++ config/database.ts
@@ -3,20 +3,20 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const sequelize = new Sequelize(
-    process.env.DATABASE_NAME,
-    process.env.USER_NAME,
-    process.env.PASSWORD,
+    process.env.DATABASE_NAME, // Tên DATABASE
+    process.env.USER_NAME,   // Tên người dùng (root)
+    process.env.PASSWORD, 
     {
-        host: process.env.DATABASE_HOST,
+        host: process.env.DATABASE_HOST,  // Link Hosting 
         port: 3306,
         dialect: 'mysql'
     }
 )
-
+// Connect Mysql
 sequelize.authenticate()
     .then(() => {
         console.log('Connect Mysql successfully!');
-    })
+    }) 
     .catch(err => {
         console.error('Connect Error!', err);
     });
--- routes/client/cart.route.ts
+++ routes/client/cart.route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 const router: Router = Router();
 
-import * as controller from "../../controllers/cart.controller";
+import controller from "../../controllers/cart.controller";
 
 router.get("/", controller.index);
 
